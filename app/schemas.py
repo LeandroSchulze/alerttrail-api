be934a583972a74ab0268841cce5a266d1b715e8
LeandroSchulze <schulzeleandro77@gmail.com>
2025-09-12 03:08:38 -0300

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,22 +1,18 @@
 from pydantic import BaseModel, EmailStr
 
-class UserCreate(BaseModel):
-    name: str
-    email: EmailStr
-    password: str
-
 class UserOut(BaseModel):
     id: int
-    name: str
     email: EmailStr
+    name: str
     plan: str
     class Config:
-        from_attributes = True
+        orm_mode = True
 
-class LoginRequest(BaseModel):
+class RegisterIn(BaseModel):
+    name: str
     email: EmailStr
     password: str
 
-class Token(BaseModel):
-    access_token: str
-    token_type: str
+class LoginIn(BaseModel):
+    email: EmailStr
+    password: str
