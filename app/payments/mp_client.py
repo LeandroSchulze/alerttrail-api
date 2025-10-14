54b099dd2f7005cd921205f59fe86791f35755d3
LeandroSchulze <schulzeleandro77@gmail.com>
2025-10-14 04:13:21 -0300

    Update mp_client.py

--- app/payments/mp_client.py
+++ app/payments/mp_client.py
@@ -1,3 +1,4 @@
+# app/payments/mp_client.py
 import os
 import mercadopago
 
@@ -5,4 +6,5 @@ MP_ACCESS_TOKEN = os.getenv("MP_ACCESS_TOKEN")
 if not MP_ACCESS_TOKEN:
     raise RuntimeError("MP_ACCESS_TOKEN no configurado")
 
+# SDK oficial de Mercado Pago
 sdk = mercadopago.SDK(MP_ACCESS_TOKEN)
