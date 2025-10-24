13bf669d804f4c21a0fb71071a44eaea4e01e39d
LeandroSchulze <schulzeleandro77@gmail.com>
2025-10-24 14:44:06 -0300

    Update merge_20251024_unify_heads.py

--- alembic/versions/merge_20251024_unify_heads.py
+++ alembic/versions/merge_20251024_unify_heads.py
@@ -1,19 +1,21 @@
-# alembic/versions/merge_20251024_unify_heads.py
+"""Merge heads to unify Alembic history"""
+
 from alembic import op
 import sqlalchemy as sa
 
 # Este archivo MERGE une los dos heads en uno solo.
 revision = "merge_20251024_unify_heads"
 
-# Reemplazá REV_ANTERIOR_REAL por el revision id real del head anterior.
-# El otro head es el de este repo: "xxxx_unique_payment_id".
-down_revision = ("REV_ANTERIOR_REAL", "xxxx_unique_payment_id")
+# Los dos heads reales a unificar:
+down_revision = ("20251013_add_trial_fields", "xxxx_unique_payment_id")
 
 branch_labels = None
 depends_on = None
 
+
 def upgrade():
     pass
+
 
 def downgrade():
     pass
