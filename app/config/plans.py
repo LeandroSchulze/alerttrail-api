2fc0ccd2695d3d1e34c8d624caef97cc71b4544d
LeandroSchulze <schulzeleandro77@gmail.com>
2026-01-13 02:26:49 -0300

    Create plans.py

---
+++ app/config/plans.py
@@ -0,0 +1,16 @@
+# app/config/plans.py
+
+PLANS = {
+    "FREE": {
+        "seats": 1,
+        "price_usd": 0,
+    },
+    "PRO": {
+        "seats": 3,
+        "price_usd": 19,
+    },
+    "BIZ": {
+        "seats": 25,
+        "price_usd": 99,
+    },
+}
